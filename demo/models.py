83548a871623e7e22beffe64679dcf7a6b6e0be2
kudinovamo <kudinovamo@mer.ci.nsu.ru>
2021-02-12 14:49:41 +0700

    Create admin, added new fields

--- demo/models.py
+++ demo/models.py
@@ -3,4 +3,9 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=40)
+    title = models.CharField(max_length=40, blank=False, unique=True)
+    description = models.TextField(max_length=500, blank=True)
+    price = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    is_published = models.BooleanField(default=True)
+    published = models.DateField()
+    cover = models.ImageField(upload_to='covers/')
